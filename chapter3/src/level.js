a42e246cb9721e5ed13e1bdff33515b8a5322efa
이기현(goorm) <gihyeon.lee@goorm.io>
2020-08-11 08:02:10 +0000

    feat(level): 설명 주석 추가

--- chapter3/src/level.js
+++ chapter3/src/level.js
@@ -1,5 +1,15 @@
 const { SUBJECT } = require('./subject');
 
+/*
+    input 
+        string subject: 과목
+            ex) SUBJECT.MATH
+        int socre: 점수
+            ex) 30
+    output
+        string level: 레벨('A', 'B', 'C', 'P')
+            ex) 'A'
+*/
 exports.genLevelBySubjectAndScore = (subject, score) => {
     
 };
